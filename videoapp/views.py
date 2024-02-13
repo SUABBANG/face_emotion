79b63e87ca8d9b55f22c39522b88a23a70c4a685
ssuangg <115128140+ssuangg@users.noreply.github.com>
2024-02-13 11:01:32 +0900

    file

--- videoapp/views.py
+++ videoapp/views.py
@@ -1,9 +1,22 @@
 import os
 
+from keras.models import load_model
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser
 from django.http import JsonResponse
 from django.conf import settings
+
+from src.utils.datasets import get_labels
+from src.utils.inference import load_detection_model
+
+# loading models
+detection_model_path = './model/haarcascade_frontalface_default.xml'
+emotion_model_path = './model/fer2013_mini_XCEPTION.102-0.66.hdf5'
+emotion_labels = get_labels('fer2013')
+
+face_detection = load_detection_model(detection_model_path)
+emotion_model_path = load_model(emotion_model_path, compile=False)
+
 class video_emotion(APIView):
     # post ~ 분석
     parser_classes = [MultiPartParser]
